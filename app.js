c271ead6472d3efbc10da39b7fe40ff80fc341f8
Senad Zukorlic <zukorlic.senad01@gmail.com>
2024-08-12 18:37:28 +0200

    instaled express.js and worked with it

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const http = require("http")
 
-const routes = require("./routes")
+const express = require("express")
 
-const server = http.createServer(routes)
+const app = express()
 
-server.listen(3000)
+app.use("/", (req, res, next) => {
+  console.log(`This always run!`)
+  next()
+})
+
+app.use("/add-product", (req, res, next) => {
+  console.log(`In the another middleware!`)
+  res.send("<h1>The 'Add Product Page'<h1>")
+})
+
+app.use("/", (req, res, next) => {
+  console.log(`In the another middleware!`)
+  res.send("<h1>Hello from Exporess!<h1>")
+})
+
+app.listen(3000)
