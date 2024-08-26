465663a79cb5e59ac8cf6ff0a920e35d3ed4a1e5
Senad Zukorlic <zukorlic.senad01@gmail.com>
2024-08-26 16:26:30 +0200

    passing data with post request and adding a cart model

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require("../models/product")
-
+const Cart = require("../models/cart")
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render("shop/product-list", {
@@ -13,10 +13,12 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId
   Product.findById(prodId, (product) => {
-    console.log(product)
+    res.render("shop/product-detail", {
+      product: product,
+      pageTitle: product.title,
+      path: "/products",
+    })
   })
-
-  res.redirect("/")
 }
 
 exports.getIndex = (req, res, next) => {
@@ -34,6 +36,14 @@ exports.getCart = (req, res, next) => {
     path: "/cart",
     pageTitle: "Your Cart",
   })
+}
+
+exports.postCard = (req, res, next) => {
+  const prodId = req.body.productId
+  Product.findById(prodId, (product) => {
+    Cart.addProduct(prodId, product.price)
+  })
+  res.redirect("/cart")
 }
 
 exports.getOrders = (req, res, next) => {
