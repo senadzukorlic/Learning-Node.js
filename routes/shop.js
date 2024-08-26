174a35238f0ba655fa8fb61e244124f1ee918b1c
Senad Zukorlic <zukorlic.senad01@gmail.com>
2024-08-26 16:26:30 +0200

    passing data with post request and adding a cart model

--- routes/shop.js
+++ routes/shop.js
@@ -13,6 +13,7 @@ router.get("/products", shopController.getProducts)
 router.get("/products/:productId", shopController.getProduct)
 
 router.get("/cart", shopController.getCart)
+router.post("/cart", shopController.postCard)
 
 router.get("/orders", shopController.getOrders)
 
