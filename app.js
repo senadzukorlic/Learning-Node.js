84eea7e442648f1f07eeaa44e512a204fb28721e
Senad Zukorlic <zukorlic.senad01@gmail.com>
2024-08-27 15:18:58 +0200

    crated mySQL server and table,conneted my app to the database,retrivering and fetchinf products

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
-const path = require('path');
+const path = require("path")
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express")
+const bodyParser = require("body-parser")
 
-const errorController = require('./controllers/error');
+const errorController = require("./controllers/error")
+const db = require("./util/database")
 
-const app = express();
+const app = express()
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs")
+app.set("views", "views")
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin")
+const shopRoutes = require("./routes/shop")
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+db.execute("SELECT * FROM products")
+  .then((res) => {
+    console.log(res[0], res[1])
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, "public")))
 
-app.use(errorController.get404);
+app.use("/admin", adminRoutes)
+app.use(shopRoutes)
 
-app.listen(3000);
+app.use(errorController.get404)
+
+app.listen(3000)
