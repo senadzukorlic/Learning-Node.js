794659ed95b8b12be9c8eaab578e95f8ae86592e
Senad Zukorlic <zukorlic.senad01@gmail.com>
2024-08-28 16:25:02 +0200

    learned how to inserting data and creating a Product with controller by using sequelize

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,15 +13,14 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl
   const price = req.body.price
   const description = req.body.description
-  const product = new Product(null, title, imageUrl, description, price)
-  product
-    .save()
-    .then(() => {
-      res.redirect("/")
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+  Product.create({
+    title: title,
+    imageUrl: imageUrl,
+    price: price,
+    description: description,
+  })
+    .then((res) => console.log(`Created product`))
+    .catch((err) => console.log(err))
 }
 
 exports.getEditProduct = (req, res, next) => {
