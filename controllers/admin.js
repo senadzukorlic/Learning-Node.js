785f9d88bd7993cef30b43ffeee2f1514e88eddb
Senad Zukorlic <zukorlic.senad01@gmail.com>
2024-09-10 15:46:42 +0200

    wrote simple instructions for understanding admin functions

--- controllers/admin.js
+++ controllers/admin.js
@@ -5,7 +5,7 @@ exports.getAddProduct = (req, res, next) => {
     //Nebitno je ime view fajlova
     pageTitle: "Add Product",
     path: "/admin/add-product",
-    editing: false, //Varijabla pomocu koje se vrsi kondicionalni rendering
+    editing: false, //Varijabla pomocu koje se vrsi kondicionalni rendering i prikazuje html/ejs fajl koji je forma za kreiranje produkta
   })
 }
 
@@ -23,7 +23,6 @@ exports.postAddProduct = (req, res, next) => {
       description: description,
     })
     .then((result) => {
-      // console.log(result);
       console.log("Created Product")
       res.redirect("/admin/products")
     })
@@ -39,10 +38,9 @@ exports.getEditProduct = (req, res, next) => {
   }
   const prodId = req.params.productId
   req.user
-    .getProducts({ where: { id: prodId } })
-
+    .getProducts({ where: { id: prodId } }) //Takodje funckija koja pripada Sequelize,"{ where: { id: prodId } }",je deo koji sluzi za "filtriranje" produkta koji se preuzimaju,samo ako je id jednak pordId.Sto znaci da ce se preuzeti samo produkt na ciji edit button je kliknuto
     .then((products) => {
-      const product = products[0]
+      const product = products[0] //getProducts funckija uvek vraca niz,zato je potrebno izdvojiti produkt koji zelimo izmeniti,formulacijom products[0].Takodje ova formulacija se vrsi zato sto moze biti da ima vise proukata sa istim id,a nama je potreban prvi u ovom slucaju
       if (!product) {
         return res.redirect("/")
       }
@@ -57,18 +55,18 @@ exports.getEditProduct = (req, res, next) => {
 }
 
 exports.postEditProduct = (req, res, next) => {
-  const prodId = req.body.productId
-  const updatedTitle = req.body.title
+  const prodId = req.body.productId //U ovom slucaju se "id" uzima iz body,a ne iz params jer je u pitanju post metoda,pa se zajedno sa podacima koji se salju na server,u body nalazi i "id"servera.Id je postavljen u vidu inputa sa type="hidden",kako nebi bio vidljiv
+  const updatedTitle = req.body.title //Nakon unsoa izmenjena,izmene ostaju u html/ejs kodu,pa zato pristupamo sa parametrom body,da bi dosli do izmena i radili sa njima
   const updatedPrice = req.body.price
   const updatedImageUrl = req.body.imageUrl
   const updatedDesc = req.body.description
-  Product.findByPk(prodId)
+  Product.findByPk(prodId) //Da bi sacuvali izmene koje je korisnik napravio,moramo pristupiti tom proizvodu u bazi podataka(gde ga cuvamo) i rucno sacuvati izmene na dole prikazan nacin
     .then((product) => {
       product.title = updatedTitle
       product.price = updatedPrice
       product.description = updatedDesc
       product.imageUrl = updatedImageUrl
-      return product.save()
+      return product.save() //Sequelize metoda koja cuva u bazi podataka
     })
     .then((result) => {
       console.log("UPDATED PRODUCT!")
@@ -94,7 +92,7 @@ exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId
   Product.findByPk(prodId)
     .then((product) => {
-      return product.destroy()
+      return product.destroy() //Takodje Sequelize funkcija koja sluzi za brisanje iz baze
     })
     .then((result) => {
       console.log("DESTROYED PRODUCT")
@@ -102,3 +100,5 @@ exports.postDeleteProduct = (req, res, next) => {
     })
     .catch((err) => console.log(err))
 }
+
+//Vazno napomenuti da ovakakv nacin pristupanja elementima u bazi podataka:"req.user.getProducts({ where: { id: prodId } })" je isti kao i ovaj nacin:"Product.findByPk(prodId)"
