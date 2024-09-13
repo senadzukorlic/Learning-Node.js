038bd003551f094cb2a5aa4a4e70a942055c26f3
Senad Zukorlic <zukorlic.senad01@gmail.com>
2024-09-14 00:14:21 +0200

    fixed some bugs with database

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const User = require("../models/user")
+
 exports.getLogin = (req, res, next) => {
   // const isLoggedIn = req.get("Cookie").split(";")[0].trim().split("=")[0]
   res.render("auth/login", {
@@ -8,13 +10,18 @@ exports.getLogin = (req, res, next) => {
 }
 
 exports.postLogin = (req, res, next) => {
-  req.session.isLoggedIn = true
-  res.redirect("/")
+  User.findByPk(1)
+    .then((user) => {
+      req.session.isLoggedIn = true
+      req.session.user = user
+      res.redirect("/")
+    })
+    .then((err) => console.log(err))
 }
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy((err) => {
     console.log(err)
+    res.redirect("/")
   })
-  res.redirect("/")
 }
