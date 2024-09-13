b8e4940d9139287eb6c7a15434ea43000ddb34a1
Senad Zukorlic <zukorlic.senad01@gmail.com>
2024-09-13 15:19:06 +0200

    initializing and using session middleware

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require("path")
 
 const express = require("express")
 const bodyParser = require("body-parser")
+const session = require("express-session")
 
 const errorController = require("./controllers/error")
 const sequelize = require("./util/database")
@@ -23,6 +24,9 @@ const authRoutes = require("./routes/auth")
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, "public")))
+app.use(
+  session({ secret: "my secret", resave: false, saveUninitialized: false })
+)
 
 app.use((req, res, next) => {
   User.findByPk(1)
@@ -36,7 +40,6 @@ app.use((req, res, next) => {
 app.use("/admin", adminRoutes)
 app.use(shopRoutes)
 app.use(authRoutes)
-
 app.use(errorController.get404)
 
 Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" })
