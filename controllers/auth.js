33b6b2c85db1d619aee0b03e94e9de44649c595b
Senad Zukorlic <zukorlic.senad01@gmail.com>
2024-09-16 15:31:49 +0200

    created views with ejs for sign in route

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,16 @@
 const User = require("../models/user")
 
 exports.getLogin = (req, res, next) => {
-  // const isLoggedIn = req.get("Cookie").split(";")[0].trim().split("=")[0]
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
+    isAuthenticated: false,
+  })
+}
+exports.getSignup = (req, res, next) => {
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Signup",
     isAuthenticated: false,
   })
 }
@@ -13,6 +19,8 @@ exports.postLogin = (req, res, next) => {
   req.session.isLoggedIn = true
   res.redirect("/")
 }
+
+exports.postSignup = (req, res, next) => {}
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy((err) => {
